97a4f359e6353e968303e3631cb4220004158290
mikaelleRubia <mikaelle.rubia@outlook.com>
2023-09-07 13:59:29 -0300

    feature: atividade 02, letra a

---
+++ exercicio02-a.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(void){
+
+ 
+        cout << "O numero " << 0 << " corresponde ao valor " << int('0') << endl;
+        cout << "O numero " << 1 << " corresponde ao valor " << int('1') << endl;
+        cout << "O numero " << 2 << " corresponde ao valor " << int('2') << endl;
+        cout << "O numero " << 3 << " corresponde ao valor " << int('3') << endl;
+        cout << "O numero " << 4 << " corresponde ao valor " << int('4') << endl;
+        cout << "O numero " << 5 << " corresponde ao valor " << int('5') << endl;
+        cout << "O numero " << 6 << " corresponde ao valor " << int('6') << endl;
+        cout << "O numero " << 7 << " corresponde ao valor " << int('7') << endl;
+        cout << "O numero " << 8 << " corresponde ao valor " << int('8') << endl;
+        cout << "O numero " << 9 << " corresponde ao valor " << int('9') << endl;
+
+    return 0;
+
+}
