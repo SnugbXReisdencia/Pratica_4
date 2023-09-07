63e43ceefba3dfe11323f631b72572bee67140ad
mikaelleRubia <mikaelle.rubia@outlook.com>
2023-09-07 14:26:07 -0300

    feature: exercicio 3 letra a

---
+++ exercicio03-a.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+#include <limits.h>
+
+using namespace std;
+
+int main() {
+
+    cout << "Tamanho Maximo: " << INT_MAX << endl;
+    cout << "Tamanho Minimo: " << INT_MIN << endl;
+
+    return 0;
+}
